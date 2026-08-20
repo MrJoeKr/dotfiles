8ad747cd37fca05fbfad3905bacdac160f84158a
MrJoeKr <kraus.jozef225@gmail.com>
2026-08-20 15:37:53 +0200

    Add packages, fish, KDE Plasma

---
+++ TODO.md
@@ -0,0 +1,7 @@
+# TODO
+
+## Fish
+
+[ ] Add fish functions and completions (if I create custom ones later?)
+[ ] Drop `source /usr/share/cachyos-fish-config/cachyos-config.fish` from config.fish, replace with config
+
---
+++ dot_config/fish/config.fish
@@ -0,0 +1,15 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+
+# Safe rm
+alias rmi='rm -i'
+
+# vim to nvim
+alias vim='nvim'
+
+set -gx EDITOR "nvim"
